c0cac466cce8a8676bfa02d361ddfec2fe3090e2
marawan <marawan1805@gmail.com>
2023-02-10 12:54:00 +0200

    Update Circle.js

--- screens/Circle.js
+++ screens/Circle.js
@@ -3,18 +3,37 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import GroupChat from "./GroupChat";
 import GroupMeet from "./GroupMeet";
+import { Ionicons, Entypo } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 const Circle = () => {
   return (
     <Tab.Navigator
-      screenOptions={{
+      screenOptions={({ route }) => ({
         headerShown: false,
-      }}
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          let iconColor;
+          if (route.name === "Chat") {
+            iconName = focused
+              ? "ios-chatbubble-sharp"
+              : "ios-chatbubble-outline";
+            iconColor = focused ? "blue" : "gray";
+          } else if (route.name === "Meet") {
+            iconName = focused ? "people-sharp" : "people-outline";
+            iconColor = focused ? "blue" : "gray";
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={24} color={iconColor} />;
+        },
+        tabBarActiveTintColor: "blue",
+        tabBarInactiveTintColor: "gray",
+      })}
     >
-      <Tab.Screen name="Group Chat" component={GroupChat} />
-      <Tab.Screen name="Group Meet" component={GroupMeet} />
+      <Tab.Screen name="Chat" component={GroupChat} />
+      <Tab.Screen name="Meet" component={GroupMeet} />
     </Tab.Navigator>
   );
 };
